24d3e0f17076a4a488907dc406c70428bfd5a97d
gyubhin <iami0115@naver.com>
2025-11-24 16:16:31 +0900

    🐛 fix / db 및 실행 설정 버그 수정

--- src/main/java/com/rookies4/finalProject/FinalProjectApplication.java
+++ src/main/java/com/rookies4/finalProject/FinalProjectApplication.java
@@ -2,12 +2,13 @@ package com.rookies4.finalProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.rookies4.finalProject.domain.entity")
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
-
 }
