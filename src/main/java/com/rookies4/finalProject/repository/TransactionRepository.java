31979fd6b3d8e039fb13946833ff3a948b743398
NamEon <143885031+skadjs@users.noreply.github.com>
2025-11-27 17:22:34 +0900

    feat: 주문 내역 조회 repository 추가

--- src/main/java/com/rookies4/finalProject/repository/TransactionRepository.java
+++ src/main/java/com/rookies4/finalProject/repository/TransactionRepository.java
@@ -1,12 +1,17 @@
 package com.rookies4.finalProject.repository;
 
 import com.rookies4.finalProject.domain.entity.Transaction;
-import java.util.List;
+import com.rookies4.finalProject.domain.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // 특정 사용자의 거래 내역을 최신순으로 조회
+    // 1. 사용자 ID + 주문 상태로 조회 (파라미터가 있을 때 사용)
+    List<Transaction> findByUser_IdAndStatusOrderByExecutedAtDesc(Long userId, TransactionStatus status);
+
+    // 2. 사용자 ID로 전체 조회 (파라미터가 없을 때 사용)
     List<Transaction> findByUser_IdOrderByExecutedAtDesc(Long userId);
 }
